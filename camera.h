e4cc1e703d1f62f55e7aea694f41d79c15d5d14b
Jasper <halvorsonjasper11@gmail.com>
2024-08-19 11:49:28 -0700

    Antialiasing for smooth edges

--- camera.h
+++ camera.h
@@ -9,6 +9,7 @@ class camera {
   public:
     double aspect_ratio = 1.0;  // Ratio of image width over height
     int    image_width  = 100;  // Rendered image width in pixel count
+    int samples_per_pixel = 10;
 
     void render(const hittable& world) {
         initialize();
@@ -18,12 +19,12 @@ class camera {
         for (int j = 0; j < image_height; j++) {
             std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
             for (int i = 0; i < image_width; i++) {
-                auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-                auto ray_direction = pixel_center - center;
-                ray r(center, ray_direction);
-
-                color pixel_color = ray_color(r, world);
-                write_color(std::cout, pixel_color);
+                color pixel_color(0, 0, 0);
+                for( int sample = 0; sample < samples_per_pixel; sample++){
+                    ray r = get_ray(i, j);
+                    pixel_color += ray_color(r, world);
+                }
+                write_color(std::cout, pixel_samples_scale * pixel_color);
             }
         }
 
@@ -32,6 +33,7 @@ class camera {
 
   private:
     int    image_height;   // Rendered image height
+    double pixel_samples_scale;
     point3 center;         // Camera center
     point3 pixel00_loc;    // Location of pixel 0, 0
     vec3   pixel_delta_u;  // Offset to pixel to the right
@@ -40,6 +42,8 @@ class camera {
     void initialize() {
         image_height = int(image_width / aspect_ratio);
         image_height = (image_height < 1) ? 1 : image_height;
+
+        pixel_samples_scale = 1.0 / samples_per_pixel;
 
         center = point3(0, 0, 0);
 
@@ -62,17 +66,33 @@ class camera {
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
     }
 
-        color ray_color(const ray& r, const hittable& world) const {
-            hit_record rec;
+    ray get_ray(int i, int j) const {
+        auto offset = sample_square();
+        auto pixel_sample = pixel00_loc + ((i + offset.x())*pixel_delta_u) + ((j+offset.y())*pixel_delta_v);
 
-            if (world.hit(r, interval(0, infinity), rec)) {
-                return 0.5 * (rec.normal + color(1,1,1));
-            }
+        auto ray_origin = center;
+        auto ray_direction = pixel_sample - ray_origin;
 
-            vec3 unit_direction = unit_vector(r.direction());
-            auto a = 0.5*(unit_direction.y() + 1.0);
-            return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+        return ray(ray_origin, ray_direction);
+    }
+
+    vec3 sample_square() const {
+        return vec3(random_double() -0.5, random_double() - 0.5, 0);
+    }
+    
+
+
+    color ray_color(const ray& r, const hittable& world) const {
+        hit_record rec;
+
+        if (world.hit(r, interval(0, infinity), rec)) {
+            return 0.5 * (rec.normal + color(1,1,1));
         }
+
+        vec3 unit_direction = unit_vector(r.direction());
+        auto a = 0.5*(unit_direction.y() + 1.0);
+        return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+    }
 };
 
 #endif
