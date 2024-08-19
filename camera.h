3aed2d82e3dc8827641d0665d900fa5b7623dc65
Jasper <halvorsonjasper11@gmail.com>
2024-08-19 12:32:18 -0700

    Lambertian Distribution and Gamma Correction

--- camera.h
+++ camera.h
@@ -10,6 +10,7 @@ class camera {
     double aspect_ratio = 1.0;  // Ratio of image width over height
     int    image_width  = 100;  // Rendered image width in pixel count
     int samples_per_pixel = 10;
+    int max_depth = 10;
 
     void render(const hittable& world) {
         initialize();
@@ -22,7 +23,7 @@ class camera {
                 color pixel_color(0, 0, 0);
                 for( int sample = 0; sample < samples_per_pixel; sample++){
                     ray r = get_ray(i, j);
-                    pixel_color += ray_color(r, world);
+                    pixel_color += ray_color(r, max_depth, world);
                 }
                 write_color(std::cout, pixel_samples_scale * pixel_color);
             }
@@ -82,12 +83,16 @@ class camera {
     
 
 
-    color ray_color(const ray& r, const hittable& world) const {
+    color ray_color(const ray& r, int depth, const hittable& world) const {
+        // Ray Bounce limit
+        if (depth <= 0){
+            return color(0, 0, 0);
+        }
         hit_record rec;
 
-        if (world.hit(r, interval(0, infinity), rec)) {
-            vec3 direction = random_on_hemisphere(rec.normal);
-            return 0.5 * ray_color(ray(rec.p, direction), world); // 0% would be white, 100% is black, so 50% should be a grayish colour
+        if (world.hit(r, interval(0.001, infinity), rec)) {
+            vec3 direction = rec.normal + random_unit_vector();
+            return 0.7 * ray_color(ray(rec.p, direction), depth-1, world); // 0% would be white, 100% is black, so 50% should be a grayish colour
         }
 
         vec3 unit_direction = unit_vector(r.direction());
