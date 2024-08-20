c1861b451daabd9b66d96a5bd4353289d3cce624
Jasper <halvorsonjasper11@gmail.com>
2024-08-20 13:02:11 -0700

    Reflection of rays

--- sphere.h
+++ sphere.h
@@ -6,7 +6,11 @@
 
 class sphere : public hittable{
     public:
-        sphere(const point3& center, double radius) : center(center), radius(std::fmax(0, radius)) {}
+        sphere(const point3& center, double radius, shared_ptr<material> mat)
+            : center(center), radius(std::fmax(0, radius)), mat(mat) {
+            // Initialize material pointer mat here
+
+        }
 
         bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
             vec3 oc = center - r.origin();
@@ -33,6 +37,7 @@ class sphere : public hittable{
             rec.p = r.at(rec.t);
             vec3 outward_normal = (rec.p - center) / radius;
             rec.set_face_normal(r, outward_normal);
+            rec.mat = mat;
 
             return true;
         }
@@ -40,6 +45,7 @@ class sphere : public hittable{
         private:
             point3 center;
             double radius;
+            shared_ptr<material> mat;
 };
 
 #endif
